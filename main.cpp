4e5e292607c7ead75785e1efa26446bdb4eaaf11
Karl Yerkes <karl.yerkes@gmail.com>
2023-12-10 07:03:30 -0800

    made it compile

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #define DR_WAV_IMPLEMENTATION
 #include "dr_wav.h"
 
+#define SAMPLERATE (44100)
+
 int main(int argc, char *argv[])
 {
 
@@ -40,6 +42,7 @@ int main(int argc, char *argv[])
   for (int i = 0; i < sigLength; i++)
   {
     origSig[i] = pSampleData[i];
+    std::cout << origSig[i] << std::endl;
   }
 
   // create vec for the output signal
@@ -49,7 +52,7 @@ int main(int argc, char *argv[])
   // if sample rate is 44100 a window of 300 samples would get us a little below 150hz, right?
   // may need to change this depending where samplerate ends up
   int windowSize = 300;
-  int hopSize = widowSize / 2;
+  int hopSize = windowSize / 2;
 
   // create vec for the autocorrelation vector. the length is windowSize
   arma::vec coor = arma::vec(windowSize);
@@ -57,24 +60,25 @@ int main(int argc, char *argv[])
   // outer loop for windowing
   for (int bin = 0; bin < (sigLength / hopSize); bin++)
   {
+      // temp vec for windowed signal
+      arma::vec chunk = arma :: vec(windowSize);
+
     // inner window loop
     for (int j = 0; j < windowSize; j++)
     {
 
-      // temp vec for windowed signal
-      arma::vec tempSig = arma : vec(windowSize);
 
       // current value with hann window applied
       float curHann = origSig[bin * hopSize + j] * 0.5 * (1 - std::cos(2 * M_PI * j / (windowSize - 1)));
 
       // write to temp
-      tempSig[j] = curHann;
+      chunk[j] = curHann;
     }
 
-    // perform autocorrelation on the tempSig vector
+    // perform autocorrelation on the chunk vector
     for (int lag = 0; lag < windowSize; lag++)
     {
-      coor[lag] = arma : accu(tempSig.head(windowSize - lag) % tempSig.tail(windowSize - lag));
+      coor[lag] = arma :: accu(chunk.head(windowSize - lag) % chunk.tail(windowSize - lag));
     }
 
     // turn coor into a toeplitz
@@ -92,22 +96,22 @@ int main(int argc, char *argv[])
     // perform linear regression
     int order = 10;
     mlpack::LinearRegression lr(COOR, coorRow.subvec(1, order));
-    coeff = lr.Parameters();
+    auto coeff = lr.Parameters();
 
     // find max index of coor
     int maxIndex = 0;
     float maxValue = coor[0];
-    for (int m = 1; i < windowSize; m++)
+    for (int m = 1; m < windowSize; m++)
     {
-      if (coor[i] > maxValue)
+      if (coor[m] > maxValue)
       {
-        maxValue = coor[i];
+        maxValue = coor[m];
         maxIndex = m;
       }
     }
 
     // calcate pitch in hz
-    float pitch = sampleRate / static_cast<float>(maxIndex);
+    float pitch = SAMPLERATE / static_cast<float>(maxIndex);
 
     // decide on voiced or unvoiced. I have no idea what the threshold value should. Total correlation should be windowSize
     float threshold = 150.0;
@@ -122,19 +126,19 @@ int main(int argc, char *argv[])
     {
       for (int n = 0; n < windowSize; n++)
       {
-        tempSig[n] *= std::cos(2.0 * M_PI * pitch * n);
+        chunk[n] *= std::cos(2.0 * M_PI * pitch * n);
       }
     }
 
     // apply filter coefficients as an all pole filter
-    int filterOrder = coeff.length - 1;
+    int filterOrder = coeff.size() - 1;
 
     // create temp output
     arma::vec tempOut = arma::vec(windowSize);
 
     for (int p = 0; p < windowSize; p++)
     {
-      tempOut[p] = coeff(0) * tempSig[p]; // Initialize with the direct term
+      tempOut[p] = coeff(0) * chunk[p]; // Initialize with the direct term
 
       for (int r = 1; r <= filterOrder; r++)
       {
@@ -158,11 +162,11 @@ int main(int argc, char *argv[])
   format.container = drwav_container_riff;
   format.format = DR_WAVE_FORMAT_IEEE_FLOAT;
   format.channels = 1;
-  format.sampleRate = 44100;
+  format.sampleRate = SAMPLERATE;
   format.bitsPerSample = 32;
 
   pWav = drwav_open_file_write("out.wav", &format);
-  for (double d = 0; d < newSig.length; d += 1)
+  for (double d = 0; d < newSig.size(); d += 1)
   {
     float f = newSig[d];
     drwav_write(pWav, 1, &f);
