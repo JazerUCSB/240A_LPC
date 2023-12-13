0f52ba83e0124438ca503335191d3b4636fd8eda
Jazer Sibley-Schwartz <141766786+JazerUCSB@users.noreply.github.com>
2023-12-13 10:54:19 -0800

    back to mlpack linear regression

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 
   // length of file
   int sigLength = pWav->totalPCMFrameCount;
-
+  int decLength = sigLength / 6;
   drwav_close(pWav);
 
   // make a vec to store the signal
@@ -42,8 +42,10 @@ int main(int argc, char *argv[])
   // write float array into vec
   for (int i = 0; i < sigLength; i++)
   {
-
+    // if (i % 6 == 0)
+    // {
     origSig[i] = pSampleData[i];
+    // }
     // std::cout << origSig[i] << std::endl;
   }
 
@@ -53,17 +55,17 @@ int main(int argc, char *argv[])
   // define window size
   // if sample rate is 44100 a window of 300 samples would get us a little below 150hz, right?
   // may need to change this depending where samplerate ends up
-  int windowSize = 50;
+  int windowSize = 100;
   int hopSize = windowSize / 2;
-
+  int bins = sigLength / hopSize;
   // create vec for the autocorrelation vector. the length is windowSize
   arma::vec corr = arma::vec(windowSize);
 
   // outer loop for windowing
-  for (int bin = 0; bin < (sigLength / hopSize); bin++)
+  for (int bin = 0; bin < bins; bin++)
   {
     // temp vec for windowed signal
-    arma::vec chunk = arma ::vec(windowSize);
+    arma::vec chunk = arma::vec(windowSize);
 
     // inner window loop
     for (int j = 0; j < windowSize; j++)
@@ -71,8 +73,9 @@ int main(int argc, char *argv[])
 
       // current value with hann window applied
       float curHann = origSig[bin * hopSize + j] * 0.5 * (1 - std::cos(2 * M_PI * j / (windowSize - 1)));
-
-      // write to temp
+      // std::cout << curHann << std::endl;
+      // std::cout << origSig[bin * hopSize + j] << std::endl;
+      //   write to temp
       chunk[j] = curHann;
     }
 
@@ -80,88 +83,110 @@ int main(int argc, char *argv[])
     for (int s = 0; s < windowSize; s++)
     {
       float thisCorr = 0;
-      for (int lag = 1; lag < windowSize; lag++)
+      for (int lag = 1; lag <= windowSize; lag++)
       {
-        if ((s + lag) < windowSize)
+        if ((s - lag) >= 0)
         {
-          thisCorr += chunk[s] * chunk[s + lag];
+          thisCorr += chunk[s] * chunk[s - lag];
         }
       }
       corr[s] = thisCorr;
+      // std::cout << corr[s] << std::endl;
     }
-
-    // turn corr into a toeplitz
-    arma::mat CORR = arma::toeplitz(corr);
-
-    // solve for the filter coefficients
-    arma::vec coeff = solve(CORR, corr);
 
     // find max index of corr
     int maxIndex = 0;
     float maxValue = corr[0];
-    for (int m = 1; m < windowSize; m++)
+    for (int t = 1; t < windowSize; t++)
     {
-      if (corr[m] > maxValue)
+      if (corr[t] > maxValue)
+
       {
-        maxValue = corr[m];
-        maxIndex = m;
+        maxValue = corr[t];
+        maxIndex = t;
       }
     }
 
-    // calcate pitch in hz
-    float pitch = SAMPLERATE / (static_cast<float>(maxIndex));
-    // std::cout << pitch;
-    std::cout << maxValue << std::endl;
-    //   decide on voiced or unvoiced. I have no idea what the threshold value should. Total correlation should be windowSize
-    float threshold = 0.01;
-    bool voiced = false;
+    // calculate pitch in hz
+    double pitch = SAMPLERATE / (static_cast<float>(maxIndex));
+
+    // normalize corr
+    for (int u = 0; u < windowSize; u++)
+    {
+
+      corr[u] /= maxValue;
+    }
+
+    // turn corr into a toeplitz
+    arma::mat CORR = arma::toeplitz(corr);
+    arma::rowvec corrRow = arma::rowvec(windowSize);
+    // arma::mat invCORR = arma::pinv(CORR);
+    for (int h = 0; h < windowSize; h++)
+    {
+      corrRow[h] = corr[h];
+    }
+
+    // solve for the filter coefficients
+    mlpack::LinearRegression lr(CORR, corrRow);
+    auto coeff = lr.Parameters();
+
+    // arma::vec coeff = arma::solve(CORR, corr);
+    // arma::vec coeff = invCORR * corr;
+    // calculate pitch in hz
+    double pitch = SAMPLERATE / (static_cast<float>(maxIndex));
+    // std::cout << pitch << std::endl;
+    // std::cout << maxValue << std::endl;
+    //       decide on voiced or unvoiced. I have no idea what the threshold value should. Total correlation should be windowSize
+    float threshold = .01;
+
     if (maxValue > threshold)
     {
-      voiced = true;
-    }
-
-    // if voiced multiply the temp sig by a cos wave
-    if (voiced)
-    {
-      for (int n = 0; n < windowSize; n++)
+      for (int a = 0; a < windowSize; a++)
       {
-        chunk[n] *= std::cos(2.0 * M_PI * pitch * n * (windowSize / SAMPLERATE));
+        // int rate = SAMPLERATE / pitch;
+        // int peak = (a % rate) < 4;
+        // chunk[a] = peak;
+        double wave = M_PI * static_cast<double>(pitch) * static_cast<double>(a) / (static_cast<double>(SAMPLERATE));
+        chunk[a] = std::abs(std::sin(wave));
       }
     }
-
-    // if unvoiced multiply by randu
-    else if (!voiced)
+    // if unvoiced randu
+    else if (maxValue <= threshold)
     {
-      for (int m = 0; m < windowSize; m++)
+      for (int w = 0; w < windowSize; w++)
       {
-        chunk[m] *= arma::randu() * 2.0 - 1.0;
+        chunk[w] = (arma::randu() * 2.0 - 1.0) * 0.5;
       }
     }
-
-    // apply filter coefficients as an all pole filter
+    // std::cout << voiced << std::endl;
+    //   apply filter coefficients as an all pole filter
     int filterOrder = coeff.size() - 1;
 
     // create temp output
-    arma::vec tempOut = arma::vec(windowSize);
+    arma::vec newChunk = arma::vec(windowSize);
 
     for (int p = 0; p < windowSize; p++)
     {
-      tempOut[p] = coeff[0] * chunk[p]; // Initialize with the direct term
-
+      newChunk[p] = coeff[0] * chunk[p]; // Initialize with the direct term
+      // std::cout << chunk[p] << std::endl;
       for (int r = 1; r <= filterOrder; r++)
       {
-        // Check if the index is within bounds before accessing the outputSignal vector
-        if (p >= r)
+        if (p > r)
         {
-          tempOut[p] += coeff[r] * tempOut[p - r];
+          newChunk[p] += coeff[r] * chunk[p - r];
         }
       }
+      // std::cout << newChunk[p] << std::endl;
+      // std::cout << chunk[p] << std::endl;
+      // std::cout << coeff[p] << std::endl;
     }
 
-    // write tempOut to newSig
+    // write newChunk to newSig
     for (int s = 0; s < windowSize; s++)
     {
-      newSig[hopSize * bin + s] = tempOut[s];
+      float curHann = newChunk[s] * 0.5 * (1 - std::cos(2 * M_PI * s / (windowSize - 1)));
+      newSig[hopSize * bin + s] += curHann;
+      // std::cout << newChunk[s] << std::endl;
     }
   }
 
